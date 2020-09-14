284d80ffc3c67cac2fcfb6d247ea41bd469168bd
jean.melo <jean.melo@saastec.com.br>
2020-09-14 12:42:49 -0300

    Quagga

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,41 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  <div>
+    <v-quagga :onDetected="logIt" :readerSize="readerSize" :readerTypes="['ean_reader']"></v-quagga>
   </div>
+
+  </div>
+
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+import VueQuagga from 'vue-quaggajs'
+
+Vue.use(VueQuagga)
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      readerSize: {
+        width: 440,
+        height: 680
+      },
+      detecteds: []
+    }
+  },
+  methods: {
+    logIt (data) {
+      console.log('detected', data)
+    }
   }
 }
 </script>
